96026dbd5d81c8ec947de0cf8c84cb7210ba0275
GregJr <gregasanders91@gmail.com>
2018-12-01 01:18:23 -0600

    Initial commit

---
+++ src/main/java/com/dodgerbluee/eventstatusbe/EventstatusbeApplication.java
@@ -0,0 +1,12 @@
+package com.dodgerbluee.eventstatusbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventstatusbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventstatusbeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dodgerbluee/eventstatusbe/EventstatusbeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dodgerbluee.eventstatusbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventstatusbeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
